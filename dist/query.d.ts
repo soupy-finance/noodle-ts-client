78d64078c512f262e2d13af9c486c0f2ba7e1655
soupy-spook <spook@soupy.finance>
2022-06-17 15:04:37 -0700

    Updates

--- dist/query.d.ts
+++ dist/query.d.ts
@@ -1,9 +1,13 @@
 export declare function getBridgeParams(): Promise<any>;
 export declare function getDexParams(): Promise<any>;
 export declare function getOracleParams(): Promise<any>;
+export declare function getBalance(accAddress: string, denom: string): Promise<any>;
+export declare function getBooks(market: string): Promise<any>;
 declare const _default: {
     getBridgeParams: typeof getBridgeParams;
     getDexParams: typeof getDexParams;
     getOracleParams: typeof getOracleParams;
+    getBalance: typeof getBalance;
+    getBooks: typeof getBooks;
 };
 export default _default;
--- dist/query.js
+++ dist/query.js
@@ -3,7 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.getOracleParams = exports.getDexParams = exports.getBridgeParams = void 0;
+exports.getBooks = exports.getBalance = exports.getOracleParams = exports.getDexParams = exports.getBridgeParams = void 0;
 const modules_1 = __importDefault(require("./modules"));
 async function getBridgeParams() {
     return await modules_1.default.bridge.query.queryParams();
@@ -17,8 +17,18 @@ async function getOracleParams() {
     return await modules_1.default.oracle.query.queryParams();
 }
 exports.getOracleParams = getOracleParams;
+async function getBalance(accAddress, denom) {
+    return await modules_1.default.bank.query.queryBalance(accAddress, { denom });
+}
+exports.getBalance = getBalance;
+async function getBooks(market) {
+    return await modules_1.default.dex.query.queryBooks(market);
+}
+exports.getBooks = getBooks;
 exports.default = {
     getBridgeParams,
     getDexParams,
     getOracleParams,
+    getBalance,
+    getBooks,
 };
